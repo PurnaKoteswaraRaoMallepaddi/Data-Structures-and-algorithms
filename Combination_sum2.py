cfd1a9c186ba069a0bf4411dc711490cd87449fa
purna <koteswara.rao@quantiphi.com>
2021-05-13 16:35:48 +0530

    addiing commits

--- Combination_sum2.py
+++ Combination_sum2.py
@@ -9,24 +9,24 @@ class Solution(object):
         candidates.sort()
         def finder(target,stack,candidates_):
             # print(target,stack,candidates_)
-            if target == 0:
+            if target == 0:           #This is for when we achive what we wanted
                 sums_.append(stack)
                 return
-            elif target < 0:
+            elif target < 0:          # When the target is coming to be negative
                 return
             i = 0
-            while i < len(candidates_):
+            while i < len(candidates_):    #Here while loop because we want to parse through when there is same numbe ragain
                 
                 # stack.append(val)
-                if stack and stack[-1] > candidates_[i]:
+                if stack and stack[-1] > candidates_[i]:   #This is because to remove viceversa cases such as [2,5] and [5,2]
                     i+=1
-                    while i < len(candidates_) and candidates_[i] == candidates_[i-1]:
+                    while i < len(candidates_) and candidates_[i] == candidates_[i-1]:  #Pasing through similar cases
                         i+=1
                     continue
                 else:
-                    finder(target - candidates_[i], stack + [candidates_[i]], candidates_[:i] + candidates_[i+1:])
+                    finder(target - candidates_[i], stack + [candidates_[i]], candidates_[:i] + candidates_[i+1:])   #recursion
                 i += 1
-                while i < len(candidates_) and candidates_[i] == candidates_[i-1]:
+                while i < len(candidates_) and candidates_[i] == candidates_[i-1]:   #Pasing through similar cases
                     i+=1
-        finder(target,[],candidates)
+        finder(target,[],candidates)   #Initial Call
         return sums_
